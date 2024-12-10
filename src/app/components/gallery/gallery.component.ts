d6e749c2dcf85a9169f271b387f580ef0af7f1e2
fr0ste <73338az@gmail.com>
2024-12-10 06:58:32 -0700

    Feat: se agrega formulario para playlist.

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -9,6 +9,5 @@ import { Component } from '@angular/core';
 })
 
 export class GalleryComponent {
-  //quien vaya a implementar esto revisar la documentacion de angular 18 porque cambió la forma de agregar las imágenes y no usen el amigo de arriba porque no sabe xd
-  images = ['image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg'];
+  images = ['/comming.png', '/comming.png', '/comming.png', '/comming.png'];
 }
